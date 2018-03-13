82b47b16d242c255af141053c0552b0a2f5cd5b9
Raphael Maquine <rapiner@gmail.com>
2018-03-13 02:00:09 -0400

    Adicionado recurso de uso do controle e acao de pular

--- Assets/Scripts/JogadorMovimentacao.cs
+++ Assets/Scripts/JogadorMovimentacao.cs
@@ -5,14 +5,27 @@ using UnityEngine;
 public class JogadorMovimentacao : MonoBehaviour {
 
 	public float velocidade; 
-
+	public Rigidbody rigidBody;
 	// Use this for initialization
 	void Start () {
-			
+		rigidBody = GetComponent<Rigidbody> ();		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		if(Input.GetKeyDown(KeyCode.A)){
+			rigidBody.velocity = new Vector3 (0, 2, 3);
+		}
+
+		if(Input.GetKeyDown(KeyCode.D)){
+			rigidBody.velocity = new Vector3 (0, 2, -3);
+		}
+
+		if(Input.GetKeyDown(KeyCode.Space)){
+			rigidBody.velocity = new Vector3 (0, 5, 0);
+		}
+
 		transform.Translate (Vector3.forward  * velocidade * Time.deltaTime);		
 	}
 }
