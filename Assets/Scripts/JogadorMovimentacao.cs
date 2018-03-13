e8686611cd327407464371a398c6c52f9b9257c3
Raphael Maquine <rapiner@gmail.com>
2018-03-13 01:27:43 -0400

    Primeiro commit do projeto

---
+++ Assets/Scripts/JogadorMovimentacao.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JogadorMovimentacao : MonoBehaviour {
+
+	public float velocidade; 
+
+	// Use this for initialization
+	void Start () {
+			
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (Vector3.forward  * velocidade * Time.deltaTime);		
+	}
+}
